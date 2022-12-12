41cf420ac6ace35c1c0de0ab70f407a854636e56
Tsangington <harry.tsang@code.berlin>
2022-12-12 18:47:45 +0100

    added responsive NavBar to template

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p> Login </p>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
