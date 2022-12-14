282bf8b3bcd8956a284cf41b319553ed52d5be05
Tsangington <harry.tsang@code.berlin>
2022-12-14 18:42:43 +0100

    Added Post request to signUp and Login

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET' , 'POST'])
 def login():
-    return render_template("login.html")
+
+    return render_template("login.html", text = "Testing")
 
 
 @auth.route('/logout')
@@ -13,6 +14,23 @@ def logout():
     return "<p> Logout </p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET' , 'POST'])
 def sign_up():
-    return "<p> Sign Up </p>"
+    if request.method == 'POST':
+            email = request.form.get('email')
+            firstName = request.form.get('firstName')
+            password = request.form.get('password')
+            confirmPassword = request.form.get('confirmPassword')
+    if len(email) < 4:
+        flash('Email is too short', category = 'error')
+    elif len(firstName) < 2:
+        flash('First name too short', category = 'error')
+    elif password != confirmPassword:
+        flash("Passwords don't match", category = 'error')
+    elif len(password) < 7:
+        flash("Password is too short", category = 'error')
+    else:
+        #user passed all checks
+        flash("Account created!", category = 'success')
+        pass
+    return render_template("signUp.html")
