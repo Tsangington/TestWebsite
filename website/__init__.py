4aac05b0750eaad88e5dcd015e6290ee0ac75d57
Tsangington <harry.tsang@code.berlin>
2022-12-18 23:39:13 +0100

    Added database creation, and creating an account and login function

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+dbName = "database.db"
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 def create_app():
-    app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'QWERTYUIOP'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{dbName}'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods = ['GET' , 'POST'])
 def login():
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                print('Logged in.')
+            else:
+                flash('Incorrect Password, please try again', category='error')
+        else:
+            flash('Email does not exist', category='error')
 
     return render_template("login.html", text = "Testing")
 
@@ -16,21 +33,34 @@ def logout():
 
 @auth.route('/sign-up', methods = ['GET' , 'POST'])
 def sign_up():
+
     if request.method == 'POST':
-            email = request.form.get('email')
-            firstName = request.form.get('firstName')
-            password = request.form.get('password')
-            confirmPassword = request.form.get('confirmPassword')
-    if len(email) < 4:
-        flash('Email is too short', category = 'error')
-    elif len(firstName) < 2:
-        flash('First name too short', category = 'error')
-    elif password != confirmPassword:
-        flash("Passwords don't match", category = 'error')
-    elif len(password) < 7:
-        flash("Password is too short", category = 'error')
-    else:
-        #user passed all checks
-        flash("Account created!", category = 'success')
-        pass
+
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email is too short', category = 'error')
+        elif len(firstName) < 2:
+            flash('First name too short', category = 'error')
+        elif password != confirmPassword:
+            flash("Passwords don't match", category = 'error')
+        elif len(password) < 7:
+            flash("Password is too short", category = 'error')
+        else:
+            #user passed all checks
+            new_user = User(email=email, firstName=firstName, lastName=lastName, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            
+            flash("Account created!", category = 'success')
+            return redirect(url_for('views.home'))
+
     return render_template("signUp.html")
